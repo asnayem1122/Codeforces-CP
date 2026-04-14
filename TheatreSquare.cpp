ee9630ed9bb09814ad9d29b02d40a32d61ee04de
asnayem1122 <asnayem1122@gmail.com>
2026-04-14 23:57:36 +0600

    Initial commit with CodeForces solutions

---
+++ TheatreSquare.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n,m,a;
+    cin>>n>>m>>a;
+    long long l = m/a;
+    long long w= n/a;
+    if(m%a > 0 && n%a > 0){ 
+        l++;w++;}
+    else if(m%a > 0 && n%a == 0){l++;}
+    else if(m%a == 0 && n%a > 0){w++;}
+
+    cout << l*w;
+
+    return 0;
+
+}
