8e99861cf859b9ecf77e8e3e78c6a89511a9fe5d
asnayem1122 <asnayem1122@gmail.com>
2026-04-29 16:26:39 +0600

    Solved -- 282A

--- Bit++.cpp
+++ Bit++.cpp
@@ -1,21 +1,26 @@
 // Problem Code: 282A
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int t;
-    cin >> t;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
     int sum = 0;
+
     while(t--){
         string s;
-        cin>>s;
+        cin >> s;
 
-        if(s == "X++" || s == "++X"){
-            sum++;
+        if(s == "++X" || s == "X++"){
+            sum ++;
         }else{
-            sum--;
+            sum --;
         }
     }
-    cout<<sum<<endl;
+
+    cout << sum <<'\n';
+
     return 0;
 }
