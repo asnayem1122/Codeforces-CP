ca0848e55e7f67d69a997e68b3ba9d011825b901
asnayem1122 <asnayem1122@gmail.com>
2026-04-30 21:43:08 +0600

    solved -- 2227A

---
+++ Koshary.cpp
@@ -0,0 +1,23 @@
+//Problem Code: 2227A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+
+        if (x % 2 == 1 && y % 2 == 1)
+            cout << "NO\n";
+        else
+            cout << "YES\n";
+    }
+
+}
--- TheatreSquare.cpp
+++ TheatreSquare.cpp
@@ -1,17 +1,17 @@
 // Problem Code: 1A
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
+ 
     long long n,m,a;
     cin>>m>>n>>a;
-
+ 
     long long l= m/a;
     long long w= n/a;
-
+ 
     if(m%a>0 && n%a>0){
         l++;
         w++;
