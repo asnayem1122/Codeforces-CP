fdfb7cd258967597410939869e4bab14c49039d6
asnayem1122 <asnayem1122@gmail.com>
2026-05-05 21:29:34 +0600

    Solved -- 1866A

---
+++ Ambitious_Kid.cpp
@@ -0,0 +1,22 @@
+//Problem Code: 1866A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n;
+    cin>>n;
+    vector<long long> arr (n);
+    long long Min = LONG_LONG_MAX;
+    for(int i = 0; i<n; i++){
+        long long x;
+        cin>>x;
+        Min = min(Min, abs(x));
+    }
+
+    cout<<abs(Min)<<endl;
+
+    return 0;
+}
