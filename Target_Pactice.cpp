b23139dc4d1456e148db5e660081b6dd3fa06a57
asnayem1122 <asnayem1122@gmail.com>
2026-05-05 21:27:04 +0600

    Solved -- 1862B

---
+++ Target_Pactice.cpp
@@ -0,0 +1,24 @@
+//Practice Code: 1873C
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    vector<int>arr;
+    
+    while(cin>>x){
+        arr.push_back(x);
+    }
+
+    //print
+    for(auto &i : arr){
+        cout<<i<<" ";
+    }
+    
+
+    return 0;
+
+    //print witout ;
+
+    if(cout<<"Jello"){}
+}
