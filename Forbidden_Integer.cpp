70ab877f0784585447362655ca59b3369f6700a2
asnayem1122 <asnayem1122@gmail.com>
2026-05-09 20:20:32 +0600

    Solved -- 1845A

--- Forbidden_Integer.cpp
+++ Forbidden_Integer.cpp
@@ -1,78 +1,41 @@
 //Problem Code: 1845A
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin >> t;
 
-    while(t--)
-    {
+    while(t--) {
         int n, k, x;
         cin >> n >> k >> x;
 
-       
-        if(x != 1)
-        {
-            cout << "YES" << endl;
-            cout << n << endl;
-
-            for(int i = 0; i < n; i++)
-            {
-                cout << 1 << " ";
-            }
-            cout << endl;
+        if(x != 1) {
+            cout << "YES\n" << n << "\n";
+            for(int i = 0; i < n; i++) cout << 1 << " ";
+            cout << "\n";
         }
-
-        else
-        {
-           
-            if(k == 1)
-            {
-                cout << "NO" << endl;
-            }
-
-            else
-            {
-                
-                if(n % 2 == 0)
-                {
-                    cout << "YES" << endl;
-                    cout << n / 2 << endl;
-
-                    for(int i = 0; i < n / 2; i++)
-                    {
-                        cout << 2 << " ";
-                    }
-
-                    cout << endl;
-                }
-
-                
-                else
-                {
-                    if(k >= 3)
-                    {
-                        cout << "YES" << endl;
-
-                        int cnt = (n - 3) / 2 + 1;
-                        cout << cnt << endl;
-
-                        for(int i = 0; i < (n - 3) / 2; i++)
-                        {
-                            cout << 2 << " ";
-                        }
-
-                        cout << 3 << endl;
-                    }
-
-                    else
-                    {
-                        cout << "NO" << endl;
-                    }
-                }
-            }
+        else if(k == 1) {
+            cout << "NO\n";
+        }
+        else if(n % 2 == 0) {
+            cout << "YES\n" << n / 2 << "\n";
+            for(int i = 0; i < n / 2; i++) cout << 2 << " ";
+            cout << "\n";
+        }
+        else if(k >= 3) {
+            int cnt = (n - 3) / 2 + 1;
+            cout << "YES\n" << cnt << "\n";
+            for(int i = 0; i < (n - 3) / 2; i++) cout << 2 << " ";
+            cout << 3 << "\n";
+        }
+        else {
+            cout << "NO\n";
         }
     }
+
+    return 0;
 }
